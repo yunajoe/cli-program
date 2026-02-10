bd8e6761d009d1e32c503430ae97b3d088cb4fd0
yunajoe <yunajoe@gmail.com>
2026-02-10 09:36:44 +0900

    feat: 로또 번호를 생성하는 random class

--- src/lotto-planet/src/model/LottoGenerator.js
+++ src/lotto-planet/src/model/LottoGenerator.js
@@ -1,3 +1,5 @@
+import Random from "../../../utils/random.js";
+
 // 사용자에게 입력받은 금액을 토대로 로또를 발행하는
 class LottoGenerator {
   static #LOTTO_PRICE = 500;
@@ -14,7 +16,8 @@ class LottoGenerator {
   // amount 에 맞게 로또 복권 장수를 발행해주는 메서드
   printIssuedLottoTickets(purchasedAmount) {
     const purchasedTickets = purchasedAmount / LottoGenerator.#LOTTO_PRICE;
-    console.log("로또 장수? ===>>", purchasedTickets);
+    const result = Random.generateRandomNumbers(1, 10);
+    console.log("result =>", result);
   }
 
   // 랜덤번호에 따라 로또 번호를 출력해주는 메서드
--- src/utils/random.js
+++ src/utils/random.js
@@ -1,18 +1,55 @@
 class Random {
-  // array 확인
-  static #validateArray;
-
   // 숫자 확인
+  static #isNumber(value) {
+    return typeof value === "number";
+  }
 
   // 최소, 최대 수
+  static #validateRange(startValue, endValue) {
+    if (!Random.#isNumber(startValue) || !Random.#isNumber(endValue)) {
+      throw new Error("[ERROR] 숫자만 입력해야 합니다.");
+    }
+    if (startValue < Number.MIN_SAFE_INTEGER) {
+      throw new Error(
+        "[ERROR] 최소 숫자는 Number.MIN_SAFE_INTEGER 보다 커야 합니다.",
+      );
+    }
+    if (endValue > Number.MAX_SAFE_INTEGER) {
+      throw new Error(
+        "[ERROR] 최대 숫자는 Number.MAX_SAFE_INTEGER 보다 커야 합니다.",
+      );
+    }
+  }
 
   /**
-   *
    * 범위가 최소 min, 최대 max까지의 숫자를 return
-   *
-   *
    */
-  static pickNumbers(min, max) {
-    return Math.random(min, max + 1);
+  static #pickRandomNumbers(min, max) {
+    Random.#validateRange(min, max);
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  }
+
+  static #shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  /**
+   * 랜덤으로 5개 나오기
+   */
+  static generateRandomNumbers(min, max) {
+    const result = [];
+    for (let i = 0; i < 5; i++) {
+      const num = Random.#pickRandomNumbers(min, max);
+      result.push(num);
+    }
+    return Random.#shuffle(result);
   }
 }
+
+export default Random;
